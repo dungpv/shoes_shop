657b5a0e576dbb3ad1cc71e2660c438867f057c1
dungpv <vietdungst@gmail.com>
2023-02-19 17:08:13 +0700

    end project

--- src/redux/configStore.tsx
+++ src/redux/configStore.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./ProductReducer/productReducer";
+import userReducer from "./UserReducer/userReducer";
 
 export const store = configureStore({
   reducer: {
     productReducer,
+    userReducer,
   },
 });
 
--- src/util/config.tsx
+++ src/util/config.tsx
@@ -12,10 +12,89 @@ export const http = axios.create({
   timeout: 30000,
 });
 
+export const settings = {
+  setStorageJson: (name: string, data: any): void => {
+    data = JSON.stringify(data);
+    localStorage.setItem(name, data);
+  },
+  setStorage: (name: string, data: string): void => {
+    localStorage.setItem(name, data);
+  },
+  getStorageJson: (name: string): any | undefined => {
+    if (localStorage.getItem(name)) {
+      const dataStore: string | undefined | null = localStorage.getItem(name);
+      if (typeof dataStore == "string") {
+        const data = JSON.parse(dataStore);
+        return data;
+      }
+      return undefined;
+    }
+    return; //undefined
+  },
+  getStore: (name: string): string | null | undefined | boolean | any => {
+    if (localStorage.getItem(name)) {
+      const data: string | null | undefined = localStorage.getItem(name);
+      return data;
+    }
+    return; //undefined
+  },
+  setCookieJson: (name: string, value: any, days: number): void => {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    value = JSON.stringify(value);
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  },
+  getCookieJson: (name: string): any => {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0)
+        return JSON.parse(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+  },
+  setCookie: (name: string, value: string, days: number): void => {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  },
+  getCookie: (name: string): string | null => {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  },
+  eraseCookie: (name: string): void => {
+    document.cookie =
+      name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  },
+  clearStorage: (name: string) => {
+    localStorage.removeItem(name);
+  },
+};
+
 // cau hinh cho tat ca request gui di
 // http.interceptors
 http.interceptors.request.use(
   (config) => {
+    config.headers = {
+      ...config.headers,
+      Authorization: "Bearer " + settings.getStore(ACCESS_TOKEN),
+    };
     // Do something before request is sent
     return config;
   },
